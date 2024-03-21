b272bcaa0389344434d3fb618b2f28785c281e6e
LutekDev <lutekdev@gmail.com>
2024-03-21 14:09:05 -0300

    21/03/24

--- py.AULAS/AndreLacono/aula.016/main.py
+++ py.AULAS/AndreLacono/aula.016/main.py
@@ -1,6 +1,9 @@
 velocidade = input("Digite a Velocidade: ")
+velocidade = int(velocidade)
 
-if velocidade > "110":
+if velocidade > 110:
     print("Velocidade Acima do Permitido!")
+elif velocidade < 60:
+    print("Favor dirigir acima de 80Km/h")    
 else:
     print("Velocidade Permitida")
